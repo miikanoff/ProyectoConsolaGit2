1237d4421ec0db046bc6b67e14b25e6cd8227d8e
miikanoff <visualneli4@gmail.com>
2024-12-16 16:22:04 +0100

    Agregar clase Tareas.java

---
+++ Tareas.java
@@ -0,0 +1,9 @@
+public class Tareas {
+    public void imprimirTareas() {
+        System.out.println("Lista de tareas: ");
+    }
+
+    public void agregarTarea() {
+        System.out.println("Tarea agregada");
+    }
+}
