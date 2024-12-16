e4c4ff43e00536a394f92765dec5ae022fb05995
Neli <visualneli4@gmail.com>
2024-12-16 17:33:25 +0100

    Create Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,9 @@
+public class Saludo {
+    public void saludoInicial() {
+        System.out.println("Bienvenido.");
+    }
+
+    public void despedida() {
+        System.out.println("Adios");
+    }
+}
